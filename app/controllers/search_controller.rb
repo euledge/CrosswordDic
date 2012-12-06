28bf1bfd0e6e223e96e5ba7a2c237bdd3a489b48
Hitoshi kuroyanagi <h-kuro@minerva.co.jp>
2012-12-07 01:01:54 +0900

    若干修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,16 +3,27 @@ require 'redis'
 
 class SearchController < ApplicationController
   def query
-    query_word=params[:query]
+    @query_word=params[:query]
 
-    # 検索キーに変換する
+    keys = get_keys(@query_word)
+
+    respond_to do |format|
+      format.html {
+        @words=REDIS.sinter(*keys)
+        @words.map!{|value| ActiveSupport::JSON.decode(value)}
+      }
+      format.json {
+        render :json => REDIS.sinter(*keys)
+      }
+    end
+  end
+
+  # 検索キーに変換する
+  def get_keys(query_word)
     keys = Array.new
     (1..query_word.size).each do |i|
       keys << "#{i}:#{query_word[i-1]}" unless query_word[i-1] == '＊'
     end
     keys << "#{query_word.size+1}:＄"
-
-    @words=REDIS.sinter(*keys)
-    @words.map!{|value| ActiveSupport::JSON.decode(value)}
   end
 end
